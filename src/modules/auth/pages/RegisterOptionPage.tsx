f998e78fabe368d92b0ff95a21b56f6f116fbd3b
sandro <assandroa95@gmail.com>
2023-03-30 07:50:48 +0700

    update page title

--- src/modules/auth/pages/RegisterOptionPage.tsx
+++ src/modules/auth/pages/RegisterOptionPage.tsx
@@ -1,7 +1,12 @@
+import React from "react";
 import { AuthLayout } from "../components/AuthLayout";
 import { RegisterOptionsCard } from "../components/RegisterOptionsCard";
 
 export const RegisterOptionPage: React.FC = () => {
+  React.useEffect(() => {
+    document.title = "Join Seetrum";
+  }, []);
+
   return (
     <AuthLayout>
       <RegisterOptionsCard />
--- src/modules/auth/pages/RegisterPage.tsx
+++ src/modules/auth/pages/RegisterPage.tsx
@@ -1,7 +1,12 @@
+import React from "react";
 import { AuthLayout } from "../components/AuthLayout";
 import { RegisterForm } from "../components/RegisterForm";
 
 export const RegisterPage: React.FC = () => {
+  React.useEffect(() => {
+    document.title = "Register";
+  }, []);
+
   return (
     <AuthLayout>
       <RegisterForm />
